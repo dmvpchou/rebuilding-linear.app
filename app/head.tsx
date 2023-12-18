0fb44c9ec1df3f79cfa89b25b13ff9eab9eb1e41
dmvpchou <im@davidchou.pro>
2023-12-18 16:32:22 +0800

    .

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title>Rebuilding Linear.app</title>
+      <title>光影珈琲</title>
       <meta
         name="description"
-        content="光影珈琲"
+        content="光影咖啡官方網站"
       />
       <meta name="robots" content="noindex" />
     </>
